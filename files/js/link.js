45eee294f9c2c6108da369de3ceee6d2c2855722
Your Name <romafon678@gmail.com>
2025-12-08 15:48:15 +0300

    add links

---
+++ files/js/link.js
@@ -0,0 +1,17 @@
+// code by  (https://github.com/1428Stef)
+// file created at 8 December 2025 3:47:09 PM (UTC+3)
+
+let teezID = document.getElementById("teez")
+teezID.addEventListener('click', function() {
+    window.open("https://nubovik01.github.io");
+});
+
+let thkID = document.getElementById("thk")
+thkID.addEventListener('click', function() {
+    window.open("https://vladthk.github.io")
+});
+
+let stefID = document.getElementById("stef")
+stefID.addEventListener('click', function() {
+    window.open("https://1428stef.github.io/WikiHachi")
+})
