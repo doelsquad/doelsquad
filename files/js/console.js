1ccf960e4d128775519c00d280469cdcf16a343f
Stef <romafon678@gmail.com>
2026-01-14 15:12:47 +0300

    remove all files JS

--- files/js/console.js
+++
@@ -1,5 +0,0 @@
-// code by qwkrtezzz (https://github.com/nubovik01)
-// file created at 6 December 2025 03:07:54 PM (UTC+3)
-console.log("[*] console.js loaded.");
-
-console.log("ты чё тут делаешь, хакер недоделанный 😭");
